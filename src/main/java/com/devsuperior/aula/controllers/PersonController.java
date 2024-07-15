e8cd431bc3494cdce6c38ef142d80151ed9d40aa
amac81 <arnaldocanelas@gmail.com>
2024-07-15 12:17:45 +0100

    project import

---
+++ src/main/java/com/devsuperior/aula/controllers/PersonController.java
@@ -0,0 +1,38 @@
+package com.devsuperior.aula.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.devsuperior.aula.dto.PersonDepartmentDTO;
+import com.devsuperior.aula.services.PersonService;
+
+@RestController
+@RequestMapping(value = "/people")
+public class PersonController{
+	
+	@Autowired
+	private PersonService service;
+	
+
+	@PostMapping
+	public ResponseEntity<PersonDepartmentDTO> insert(@RequestBody PersonDepartmentDTO dto) {
+		dto = service.insert(dto); 
+		
+		//to generate the correct HTTP response code 201 - Created
+		//good programming practicePost 
+		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(dto.getId())
+				.toUri();
+		
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	
+}
+
---
+++ src/main/java/com/devsuperior/aula/services/PersonService.java
@@ -0,0 +1,40 @@
+package com.devsuperior.aula.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.aula.dto.PersonDepartmentDTO;
+import com.devsuperior.aula.entities.Department;
+import com.devsuperior.aula.entities.Person;
+import com.devsuperior.aula.repositories.PersonRepository;
+
+
+@Service
+public class PersonService {
+	
+	@Autowired
+	private PersonRepository repository;
+	
+		
+	@Transactional
+	public PersonDepartmentDTO insert(PersonDepartmentDTO dto) {
+		Person entity = new Person(); 
+		copyDtoToEntity(dto, entity);		
+
+		entity = repository.save(entity);
+		
+		return new PersonDepartmentDTO(entity);
+	}
+		
+	private void copyDtoToEntity(PersonDepartmentDTO dto, Person entity) {
+		entity.setName(dto.getName());
+		entity.setSalary(dto.getSalary());
+		
+		Department dept = new Department();	
+		dept.setId(dto.getDepartment().getId());
+		
+		entity.setDepartment(dept);
+	}
+
+}
